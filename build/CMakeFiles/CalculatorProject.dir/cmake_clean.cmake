81defbd7b9ed7b684e153b3c049045e474da0a50
omar-shawky1 <omarshawky726@gmail.com>
2025-04-04 17:02:50 +0200

    Resolved merge problem

---
+++ build/CMakeFiles/CalculatorProject.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CalculatorProject.dir/calculator.cpp.obj"
+  "CMakeFiles/CalculatorProject.dir/calculator.cpp.obj.d"
+  "CMakeFiles/CalculatorProject.dir/test.cpp.obj"
+  "CMakeFiles/CalculatorProject.dir/test.cpp.obj.d"
+  "CalculatorProject.exe"
+  "CalculatorProject.exe.manifest"
+  "CalculatorProject.pdb"
+  "libCalculatorProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CalculatorProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CalculatorProject.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CalculatorProject.
+# This may be replaced when dependencies are built.
